df077eb518d934a26050c8670ddc78504f831fd2
Brice Laroche <laroche.brice@gmail.com>
2022-01-25 14:12:33 +0100

    back: express configuré

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,25 @@ const express = require("express")
 // création application express
 const app = express()
 
-// Application utilisera cete fonction pour tout types de requête
-app.use((req, res) =>{
+
+// Application utilisera ces fonctions pour tout types de requête
+app.use((req, res, next) =>{
+    console.log("requête reçue!")
+    next()
+})
+
+app.use((req, res, next) =>{
+    res.status(201)
+    next()
+})
+
+app.use((req, res, next) =>{
     res.json({message: "Votre requête a bien été reçue !"})
+    next()
+})
+
+app.use((res, req) =>{
+    console.log("réponse envoyée avec succès !")
 })
 
 //exportation cette app, pour qu'on puisse y accéder depuis d'autres fichiers (notamment node)
