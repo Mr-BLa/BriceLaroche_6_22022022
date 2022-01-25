25f118bf489400fd4c6298e3b6e2fbe9e0f7528e
Brice Laroche <laroche.brice@gmail.com>
2022-01-25 13:36:07 +0100

    back: exe app Express sur le serveur Node

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,10 @@ const express = require("express")
 // création application express
 const app = express()
 
+// Application utilisera cete fonction pour tout types de requête
+app.use((req, res) =>{
+    res.json({message: "Votre requête a bien été reçue !"})
+})
+
 //exportation cette app, pour qu'on puisse y accéder depuis d'autres fichiers (notamment node)
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-// Ecoute les requêtes http et y répond
+/* 
+**  Ecoute les requêtes http et y répond
+*/
+
 
 // Objet http qui permet de créer serveur
 const http = require('http')
+// Import application 
+const app = require("./app")
+
+
+//Dire à l'app, sur quel port elle va tourner
+app.set("port", process.env.PORT || 3000)
 
 // Appel méthode createServer du package http. Cette methode prend comme argument, la fonction qui sera appeler à chaque reçue par le serveur
-const server = http.createServer((req, res) => {
-    //methode "end" de l'objet réponse
-    res.end('Voilà la réponse du nodemon serveur !')
-})
+const server = http.createServer(app)
 
 //Methode listen du serveur pour écouter requête
 server.listen(process.env.PORT || 3000)
