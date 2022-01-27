13dc1f4039adb4a8b606c5155f3d93a228e32be0
Brice Laroche <laroche.brice@gmail.com>
2022-01-27 15:03:05 +0100

    app.js et sauce.js: middleware=>sauce.js = router

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,6 @@ const app = express()
 
 
 
-//Pour gérer la requête POST venant de l'application front-end, on a besoin d'en extraire le corps JSON
-app.use(express.json())
-
-
 //Middleware général, pour permettre  l'app, d'accéder  l'API sans problèmes
 app.use((req, res, next) => {
     //accéder à notre API depuis n'importe quelle origine ( '*' )
@@ -40,51 +36,9 @@ app.use((req, res, next) => {
     next()
 })
 
-app.post('/api/sauces', (req, res, next) => {
-    //Retire le champ id renvoyé par mongoDb, avant de le copier
-    delete req.body._id
-    //création nouvelle instance de notre model Sauce
-    const sauce = new Sauce({
-        //copie les champs du model Sauce, dans le body/corps de la request
-        ...req.body
-    })
-    //enregistrement du "sauce" dans la base de données
-    sauce.save()
-        .then(() => res.status(201).json({ message: "Objet Enregistré !"}))
-        .catch(error => res.status(400).json({ error }))
-})
 
-
-app.put('/api/sauces/:id', (req, res, next) => {
-    // Mettre à jour/ modifier une sauce dans la base de données, en fonction de l'id
-    Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: "Objet Modifié !"}))
-        .catch(error => res.status(400).json({ error }))
-})
-
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-    //Supprimer une sauce de la base de données, en fonction de l'id
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }))
-})
-
-
-app.get('/api/sauces/:id', (req, res, next) =>{
-    //Trouver un seul objet dans la base de données. Via objet req.param.id (car c'est un paramètre de route dynamique)
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }))
-})
-
-
-app.get("/api/sauces", (req, res) =>{
-    //renvoyer un tableau contenant toutes les Sauces
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }))
-})
+//Pour gérer la requête POST venant de l'application front-end, on a besoin d'en extraire le corps JSON
+app.use(express.json())
 
 
 //exportation cette app, pour qu'on puisse y accéder depuis d'autres fichiers (notamment node)
---
+++ backend/routes/sauce.js
@@ -0,0 +1,50 @@
+//Implémentation des routes 
+const express = require('express')
+const router = express.Router()
+
+
+router.post('/', (req, res, next) => {
+    //Retire le champ id renvoyé par mongoDb, avant de le copier
+    delete req.body._id
+    //création nouvelle instance de notre model Sauce
+    const sauce = new Sauce({
+        //copie les champs du model Sauce, dans le body/corps de la request
+        ...req.body
+    })
+    //enregistrement du "sauce" dans la base de données
+    sauce.save()
+        .then(() => res.status(201).json({ message: "Objet Enregistré !"}))
+        .catch(error => res.status(400).json({ error }))
+})
+
+
+router.put('/:id', (req, res, next) => {
+    // Mettre à jour/ modifier une sauce dans la base de données, en fonction de l'id
+    Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Objet Modifié !"}))
+        .catch(error => res.status(400).json({ error }))
+})
+
+
+router.delete('/:id', (req, res, next) => {
+    //Supprimer une sauce de la base de données, en fonction de l'id
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }))
+})
+
+
+router.get('/:id', (req, res, next) =>{
+    //Trouver un seul objet dans la base de données. Via objet req.param.id (car c'est un paramètre de route dynamique)
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }))
+})
+
+
+router.get("/", (req, res) =>{
+    //renvoyer un tableau contenant toutes les Sauces
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }))
+})
