f3315b626eafcad32e240e42113d3c463b450ad3
Brice Laroche <laroche.brice@gmail.com>
2022-01-31 14:01:12 +0100

    controllers/user.js: controllers authentification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,11 +2,31 @@
 *          CONTROLLERS AUTHENTIFICATION
 */
 
+// Import package bcrypt
+const bcrypt = require('bcrypt')
+
+// Import Model User
+const User = require('../models/User')
 
 
-// Enregistrement de nouveaux utilisateurs
+
+// Création de nouveaux utilisateurs
 exports.signup = (req, res, next) => {
-
+    // Hacher le mdp
+    bcrypt.hash(req.body.password, 10)
+        // On récupère le hash...
+        .then(hash => {
+            // ... Qu'on enregistre dans un nouvel User...
+            const user = new User({ 
+                email: req.body.email,
+                password: hash
+            })
+            //... qu'on enregistre dans la BDD
+            user.save()
+                .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+                .catch(error => res.status(400).json({ error }))
+        })
+        .catch(error => res.status(500).json({ error }))
 }
 
 // Connecter utilisateurs existants
