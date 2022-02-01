1802457c70f6fd2b3754996c0d442ac8421d59d2
Brice Laroche <laroche.brice@gmail.com>
2022-02-01 14:46:48 +0100

    middleware/auth.js + routes/sauce.js=> auth

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -9,22 +9,24 @@ const router = express.Router()
 
 //Import controllers
 const sauceCtrl = require("../controllers/sauce")
+// Import Middleware Authentification Token
+const auth = require("../middleware/auth")
 
 
 //Router POST + import et application Controller POST
-router.post('/', sauceCtrl.createSauce )
+router.post('/', auth, sauceCtrl.createSauce )
 
 //Router PUT + import et application Controller PUT
-router.put('/:id', sauceCtrl.modifySauce )
+router.put('/:id', auth, sauceCtrl.modifySauce )
 
 //Router DELETE + import et application Controller DELETE
-router.delete('/:id', sauceCtrl.deleteSauce )
+router.delete('/:id', auth, sauceCtrl.deleteSauce )
 
 //Router GET/:id + import et application Controller GET/:id
-router.get('/:id', sauceCtrl.getOneSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce)
 
 //Router GET + import et application Controller GET
-router.get("/", sauceCtrl.getAllSauces)
+router.get("/", auth, sauceCtrl.getAllSauces)
 
 
 module.exports = router
